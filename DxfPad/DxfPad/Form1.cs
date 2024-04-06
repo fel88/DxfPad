12b2a790ee41017234f7ecad99dc604cb89b794b
fel88 <15663687+fel88@users.noreply.github.com>
2024-04-06 23:52:06 +0300

    initial

---
+++ DxfPad/DxfPad/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DxfPad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {            
+            InitializeComponent();
+            Form = this;
+        }
+        public static Form1 Form;
+        
+        internal void SetStatus(string v)
+        {
+            //toolStripStatusLabel1.Text = v;
+        }
+        public event Action<ITool> ToolChanged;
+
+        public EditModeEnum EditMode;
+        ITool _currentTool;
+        public void SetTool(ITool tool)
+        {
+            _currentTool.Deselect();
+            _currentTool = tool;
+            _currentTool.Select();
+            ToolChanged?.Invoke(_currentTool);
+        }
+        public ITool CurrentTool { get => _currentTool; }
+
+    }
+    public enum EditModeEnum
+    {
+        Part, Draft, Assembly
+    }
+}
